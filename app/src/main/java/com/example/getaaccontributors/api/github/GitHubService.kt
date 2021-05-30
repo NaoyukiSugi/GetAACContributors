72a772505d37c315f2da79fec83a014d3952786c
Naoyuki Sugi <bnsgny@gmail.com>
2021-05-30 17:40:45 +0900

    add github service class

---
+++ app/src/main/java/com/example/getaaccontributors/api/github/GitHubService.kt
@@ -0,0 +1,16 @@
+package com.example.getaaccontributors.api.github
+
+import com.example.getaaccontributors.model.UserList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+
+interface GitHubService {
+
+    @GET("repositories/{repo_id}/contributors")
+    suspend fun getContributors(
+        @Path("repo_id") repoId: String,
+        @Query("page") page: Int
+    ): Response<UserList>
+}
