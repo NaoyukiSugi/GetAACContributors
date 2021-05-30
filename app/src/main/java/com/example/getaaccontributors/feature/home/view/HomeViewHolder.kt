c5a86d3006acbffce59d7d482d780a05a5fe7216
Naoyuki Sugi <bnsgny@gmail.com>
2021-05-30 22:52:26 +0900

    implement Home ViewHolderViewProxy

--- app/src/main/java/com/example/getaaccontributors/feature/home/view/HomeViewHolder.kt
+++ app/src/main/java/com/example/getaaccontributors/feature/home/view/HomeViewHolder.kt
@@ -1,9 +1,12 @@
 package com.example.getaaccontributors.feature.home.view
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
+import com.example.getaaccontributors.R
 import com.example.getaaccontributors.feature.home.contract.HomeContract
 import com.example.getaaccontributors.model.UserList
 
@@ -26,23 +29,32 @@ class HomeViewHolder(
         private val requestManager: RequestManager = Glide.with(view)
     ) : HomeContract.ViewHolderViewProxy {
 
+        private val iconIv: ImageView
+            get() = view.findViewById(R.id.icon)
+
+        private val nameTv: TextView
+            get() = view.findViewById(R.id.name)
+
+        private val contributionsTv: TextView
+            get() = view.findViewById(R.id.contributions)
+
         override fun loadIconImage(imageUrl: String) {
-            TODO("Not yet implemented")
+            requestManager.load(imageUrl).into(iconIv)
         }
 
         override fun setName(name: String) {
-            TODO("Not yet implemented")
+            nameTv.text = name
         }
 
         override fun setContributions(contributions: String) {
-            TODO("Not yet implemented")
+            contributionsTv.text = contributions
         }
 
         override fun setOnUserClickListener(
             user: UserList.User,
             listener: HomeContract.UserClickListener?
         ) {
-            TODO("Not yet implemented")
+            view.setOnClickListener { listener?.onUserClick(user) }
         }
     }
 }
