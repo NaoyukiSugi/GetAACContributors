5408f8e9d0d9c5712199c555473fb31d7da598b7
Naoyuki Sugi <bnsgny@gmail.com>
2021-06-01 01:48:06 +0900

    add-home-viewproxy

---
+++ app/src/main/java/com/example/getaaccontributors/feature/home/view/HomeViewProxy.kt
@@ -0,0 +1,111 @@
+package com.example.getaaccontributors.feature.home.view
+
+import android.view.View
+import android.widget.Button
+import androidx.annotation.VisibleForTesting
+import androidx.core.view.isVisible
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.fragment.app.Fragment
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.getaaccontributors.R
+import com.example.getaaccontributors.feature.home.contract.HomeContract
+import com.example.getaaccontributors.model.UserList
+
+class HomeViewProxy(private val fragment: Fragment) : HomeContract.ViewProxy {
+
+    @VisibleForTesting
+    internal lateinit var homeAdapter: HomeAdapter
+
+    private val recyclerView: RecyclerView?
+        get() = fragment.view?.findViewById(R.id.recycler_view)
+
+    private val errorView: View?
+        get() = fragment.view?.findViewById(R.id.error_view)
+
+    private val emptyView: View?
+        get() = fragment.view?.findViewById(R.id.empty_view)
+
+    private val refreshButton: Button?
+        get() = fragment.view?.findViewById(R.id.refresh_button)
+
+    private val swipeRefreshLayout: SwipeRefreshLayout?
+        get() = fragment.view?.findViewById(R.id.swipe_refresh_layout)
+
+    private val progressBar: ContentLoadingProgressBar?
+        get() = fragment.view?.findViewById(R.id.progress_bar)
+
+    override fun initAdapter(userClickListener: HomeContract.UserClickListener) {
+        createHomeAdapter().run {
+            homeAdapter = this
+            recyclerView?.adapter = this
+            homeAdapter.userClickListener = userClickListener
+        }
+    }
+
+    override fun initRecyclerView() {
+        recyclerView?.run { addItemDecoration(createItemDecoration()) }
+    }
+
+    override suspend fun submitData(pagingUserData: PagingData<UserList.User>) {
+        homeAdapter.submitData(pagingUserData)
+    }
+
+    override fun showRecyclerView() {
+        recyclerView?.isVisible = true
+    }
+
+    override fun hideRecyclerView() {
+        recyclerView?.isVisible = false
+    }
+
+    override fun showErrorView() {
+        errorView?.isVisible = true
+    }
+
+    override fun hideErrorView() {
+        errorView?.isVisible = false
+    }
+
+    override fun showEmptyView() {
+        emptyView?.isVisible = true
+    }
+
+    override fun hideEmptyView() {
+        emptyView?.isVisible = false
+    }
+
+    override fun showLoadingView() {
+        progressBar?.isVisible = true
+    }
+
+    override fun hideLoadingView() {
+        progressBar?.isVisible = false
+    }
+
+    override fun setOnRefreshListener(listener: HomeContract.RefreshListener) {
+        refreshButton?.setOnClickListener { listener.onRefresh() }
+        swipeRefreshLayout?.run {
+            setOnRefreshListener {
+                isRefreshing = false
+                listener.onRefresh()
+            }
+        }
+    }
+
+    override fun navigateToDetail(user: UserList.User) {
+        TODO("Not yet implemented")
+    }
+
+    @VisibleForTesting
+    internal fun createHomeAdapter() = HomeAdapter()
+
+    @VisibleForTesting
+    internal fun createItemDecoration() =
+        DividerItemDecoration(fragment.context, DividerItemDecoration.VERTICAL)
+            .apply {
+                fragment.context?.getDrawable(R.drawable.divider)?.let { setDrawable(it) }
+            }
+}
---
+++ app/src/test/java/com/example/getaaccontributors/feature/home/view/HomeViewProxyTest.kt
@@ -0,0 +1,172 @@
+package com.example.getaaccontributors.feature.home.view
+
+import android.view.View
+import android.widget.Button
+import androidx.core.view.isVisible
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.fragment.app.Fragment
+import androidx.paging.PagingData
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.example.getaaccontributors.R
+import com.example.getaaccontributors.feature.home.contract.HomeContract
+import com.example.getaaccontributors.model.UserList
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.argumentCaptor
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.spy
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
+
+internal class HomeViewProxyTest {
+
+    private lateinit var viewProxy: HomeViewProxy
+    private val recyclerView: RecyclerView = mock()
+    private val errorView: View = mock()
+    private val emptyView: View = mock()
+    private val progressBar: ContentLoadingProgressBar = mock()
+    private val refreshButton: Button = mock()
+    private val swipeRefreshLayout: SwipeRefreshLayout = mock()
+    private val view: View = mock {
+        on { findViewById<RecyclerView>(R.id.recycler_view) } doReturn recyclerView
+        on { findViewById<View>(R.id.error_view) } doReturn errorView
+        on { findViewById<View>(R.id.empty_view) } doReturn emptyView
+        on { findViewById<ContentLoadingProgressBar>(R.id.progress_bar) } doReturn progressBar
+        on { findViewById<Button>(R.id.refresh_button) } doReturn refreshButton
+        on { findViewById<SwipeRefreshLayout>(R.id.swipe_refresh_layout) } doReturn swipeRefreshLayout
+    }
+    private val fragment: Fragment = mock {
+        on { view } doReturn view
+    }
+    private val adapter: HomeAdapter = mock()
+
+    @BeforeEach
+    fun setUp() {
+        viewProxy = spy(HomeViewProxy(fragment))
+        doReturn(adapter).whenever(viewProxy).createHomeAdapter()
+    }
+
+    // region initAdapter
+    @Test
+    fun `initAdapter should set adapter into recyclerview`() {
+        val userClickListener: HomeContract.UserClickListener = mock()
+
+        viewProxy.initAdapter(userClickListener)
+
+        verify(recyclerView).adapter = adapter
+    }
+
+    @Test
+    fun `initAdapter should set listener into adapter`() {
+        val listener: HomeContract.UserClickListener = mock()
+        viewProxy.homeAdapter = adapter
+
+        viewProxy.initAdapter(listener)
+
+        verify(adapter).userClickListener = listener
+    }
+    // endregion
+
+    @Test
+    fun `initRecyclerView should call addItemDecoration`() {
+        val itemDecoration: DividerItemDecoration = mock()
+        doReturn(itemDecoration).whenever(viewProxy).createItemDecoration()
+
+        viewProxy.initRecyclerView()
+
+        verify(recyclerView).addItemDecoration(itemDecoration)
+    }
+
+    @Test
+    fun `submitData should call submitData of adapter`() {
+        runBlocking {
+            val pagingUserData: PagingData<UserList.User> = mock()
+            viewProxy.homeAdapter = adapter
+
+            viewProxy.submitData(pagingUserData)
+
+            verify(adapter).submitData(pagingUserData)
+        }
+    }
+
+    @Test
+    fun `showRecyclerView should show recyclerView`() {
+        viewProxy.showRecyclerView()
+
+        verify(recyclerView).isVisible = true
+    }
+
+    @Test
+    fun `hideRecyclerView should hide recyclerView`() {
+        viewProxy.hideRecyclerView()
+
+        verify(recyclerView).isVisible = false
+    }
+
+    @Test
+    fun `showErrorView should show errorView`() {
+        viewProxy.showErrorView()
+
+        verify(errorView).isVisible = true
+    }
+
+    @Test
+    fun `hideErrorView should hide errorView`() {
+        viewProxy.hideErrorView()
+
+        verify(errorView).isVisible = false
+    }
+
+    @Test
+    fun `showEmptyView should show emptyView`() {
+        viewProxy.showEmptyView()
+
+        verify(emptyView).isVisible = true
+    }
+
+    @Test
+    fun `hideEmptyView should hide emptyView`() {
+        viewProxy.hideEmptyView()
+
+        verify(emptyView).isVisible = false
+    }
+
+    @Test
+    fun `showLoadingView should show progressBar`() {
+        viewProxy.showLoadingView()
+
+        verify(progressBar).isVisible = true
+    }
+
+    @Test
+    fun `hideLoadingView should hide progressBar`() {
+        viewProxy.hideLoadingView()
+
+        verify(progressBar).isVisible = false
+    }
+
+    @Test
+    fun `setOnRefreshListener should set listener into refreshButton`() {
+        val refreshListener: HomeContract.RefreshListener = mock()
+
+        viewProxy.setOnRefreshListener(refreshListener)
+
+        val tapRefreshListener = argumentCaptor<View.OnClickListener> {
+            verify(refreshButton).setOnClickListener(capture())
+        }.firstValue
+        tapRefreshListener.onClick(refreshButton)
+
+        val swipeRefreshListener = argumentCaptor<SwipeRefreshLayout.OnRefreshListener> {
+            verify(swipeRefreshLayout).setOnRefreshListener(capture())
+        }.firstValue
+        swipeRefreshListener.onRefresh()
+
+        verify(refreshListener, times(2)).onRefresh()
+        verify(swipeRefreshLayout).isRefreshing = false
+    }
+}
