e51e3a0fd25722d017da43536262f5e01b8f04a4
Naoyuki Sugi <bnsgny@gmail.com>
2021-06-01 22:30:10 +0900

    add getRepos to GitHubService

--- app/src/main/java/com/example/getaaccontributors/api/github/GitHubService.kt
+++ app/src/main/java/com/example/getaaccontributors/api/github/GitHubService.kt
@@ -1,5 +1,6 @@
 package com.example.getaaccontributors.api.github
 
+import com.example.getaaccontributors.model.RepoList
 import com.example.getaaccontributors.model.UserList
 import retrofit2.Response
 import retrofit2.http.GET
@@ -13,4 +14,10 @@ interface GitHubService {
         @Path("repo_id") repoId: String,
         @Query("page") page: Int
     ): Response<UserList>
+
+    @GET("users/{user_id}/repos")
+    suspend fun getRepos(
+        @Path("repo_id") repoId: String,
+        @Query("page") page: Int
+    ): Response<RepoList>
 }
