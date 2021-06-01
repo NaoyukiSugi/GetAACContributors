2edfe83dccd142136bb925cfc45da99eb355cb4a
Naoyuki Sugi <bnsgny@gmail.com>
2021-06-02 00:02:05 +0900

    add DetailAdapter

---
+++ app/src/main/java/com/example/getaaccontributors/feature/detail/view/DetailAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.getaaccontributors.feature.detail.view
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.annotation.VisibleForTesting
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import com.example.getaaccontributors.R
+import com.example.getaaccontributors.feature.detail.contract.DetailContract
+import com.example.getaaccontributors.model.RepoList
+
+class DetailAdapter : PagingDataAdapter<RepoList.Repo, DetailViewHolder>(DIFF_CALLBACK) {
+
+    var repoClickListener: DetailContract.RepoClickListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DetailViewHolder {
+        return createDetailViewHolder(parent)
+    }
+
+    override fun onBindViewHolder(holder: DetailViewHolder, position: Int) {
+        getRepo(position)?.run { holder.bind(this, repoClickListener) }
+    }
+
+    @VisibleForTesting
+    internal fun createDetailViewHolder(parent: ViewGroup): DetailViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.view_repo, parent, false)
+        return DetailViewHolder(view)
+    }
+
+    @VisibleForTesting
+    internal fun getRepo(position: Int) = getItem(position)
+
+    companion object {
+        @VisibleForTesting
+        internal val DIFF_CALLBACK = object : DiffUtil.ItemCallback<RepoList.Repo>() {
+
+            override fun areItemsTheSame(
+                oldItem: RepoList.Repo,
+                newItem: RepoList.Repo
+            ): Boolean {
+                return oldItem.id == newItem.id
+            }
+
+            override fun areContentsTheSame(
+                oldItem: RepoList.Repo,
+                newItem: RepoList.Repo
+            ): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
