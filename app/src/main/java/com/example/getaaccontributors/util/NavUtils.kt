d1822f97e584f99eed4e9adea08e41eab7609087
shishamo <bnsgny@gmail.com>
2021-06-01 21:33:47 +0900

    implement navigateToDetail (#21)

---
+++ app/src/main/java/com/example/getaaccontributors/util/NavUtils.kt
@@ -0,0 +1,13 @@
+package com.example.getaaccontributors.util
+
+import androidx.navigation.NavController
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
+
+fun NavController.navigateSafe(navDirections: NavDirections, navOptions: NavOptions? = null) {
+    val action = currentDestination?.getAction(navDirections.actionId)
+        ?: graph.getAction(navDirections.actionId)
+    if (action != null && currentDestination?.id != action.destinationId) {
+        navigate(navDirections.actionId, navDirections.arguments, navOptions)
+    }
+}
