8f9bfa2761bb41ceed72378a12b79a5b1b39f16d
shishamo <bnsgny@gmail.com>
2021-06-05 23:10:21 +0900

    add lifecycleOwner anotations (#58)

---
+++ app/src/main/java/com/example/getaaccontributors/feature/detail/di/annotation/DetailReposLifecycleOwner.kt
@@ -0,0 +1,7 @@
+package com.example.getaaccontributors.feature.detail.di.annotation
+
+import javax.inject.Qualifier
+
+@Retention(AnnotationRetention.RUNTIME)
+@Qualifier
+annotation class DetailReposLifecycleOwner
