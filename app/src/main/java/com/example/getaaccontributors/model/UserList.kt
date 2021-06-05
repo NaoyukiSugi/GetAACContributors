52633272ecc3a1bdabda2de6b41a27eb2faddbe3
shishamo <bnsgny@gmail.com>
2021-06-05 16:18:40 +0900

    add getUser into GitHubService (#37)

--- app/src/main/java/com/example/getaaccontributors/model/UserList.kt
+++ app/src/main/java/com/example/getaaccontributors/model/UserList.kt
@@ -43,6 +43,34 @@ class UserList : ArrayList<UserList.User>() {
         @SerializedName("site_admin")
         val siteAdmin: Boolean,
         @SerializedName("contributions")
-        val contributions: Int
+        val contributions: Int,
+        @SerializedName("name")
+        val name: String,
+        @SerializedName("company")
+        val company: String,
+        @SerializedName("blog")
+        val blog: String,
+        @SerializedName("location")
+        val location: String,
+        @SerializedName("email")
+        val email: Any?,
+        @SerializedName("hireable")
+        val hireable: Any?,
+        @SerializedName("bio")
+        val bio: Any?,
+        @SerializedName("twitter_username")
+        val twitterUsername: String,
+        @SerializedName("public_repos")
+        val publicRepos: Int,
+        @SerializedName("public_gists")
+        val publicGists: Int,
+        @SerializedName("followers")
+        val followers: Int,
+        @SerializedName("following")
+        val following: Int,
+        @SerializedName("created_at")
+        val createdAt: String,
+        @SerializedName("updated_at")
+        val updatedAt: String
     ) : Serializable
 }
